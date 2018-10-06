89865a7af837f70d23859adb1de6f596d1e36ebb
Frikk Herding Solberg <frikk@meetviva.com>
2018-10-06 23:34:31 +0200

    Made kiteReport an actual kite report

--- src/components/kiteReport.jsx
+++ src/components/kiteReport.jsx
@@ -1,100 +1,33 @@
 import React, { Component } from "react";
-import $ from "jquery";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip
-} from "recharts";
+import VindsidenReport from "./vindsidenReport";
+import YrWindReport from "./yrWindReport";
 
 class KiteReport extends Component {
   state = {
-    spotId: 3,
-    name: 0,
-    windData: []
+    spotName: "",
+    vindsidenId: 3,
+    yrId: 0
   };
 
-  render() {
+  constructor(props) {
+    super(props);
+    this.state.spotName = props.spotName;
+    this.state.vindsidenId = props.vindsidenId;
+    this.state.yrId = props.yrId;
+  }
+
+  getKiteReport() {
     return (
-      <React.Fragment>
-        <h4>{this.state.name}</h4>
-        <LineChart
-          width={800}
-          height={300}
-          data={this.state.windData}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-        >
-          <CartesianGrid strokeDasharray="10 10" />
-          <XAxis dataKey="time" />
-          <Tooltip />
-          <Line
-            type="monotone"
-            dataKey="windMin"
-            stackId="1"
-            stroke="#004D00"
-            fill="#FFFFFF"
-          />
-          <Line
-            type="monotone"
-            dataKey="windAvg"
-            stackId="1"
-            stroke="#004D00"
-            fill="#009900"
-          />
-          <Line
-            type="monotone"
-            dataKey="windMax"
-            stackId="1"
-            stroke="#004D00"
-            fill="#009900"
-          />
-          <YAxis type="number" domain={["dataMin", 1]} />
-        </LineChart>
-      </React.Fragment>
+      <div>
+        <h1>Kitereport from {this.state.spotName}</h1>
+        <VindsidenReport spotId={this.state.vindsidenId} />
+        <YrWindReport spotId={this.state.yrId} />
+      </div>
     );
   }
 
-  componentDidMount() {
-    this.getReportFromVindsiden();
-  }
-
-  getReportFromVindsiden() {
-    $.ajax({
-      type: "GET",
-      url:
-        "http://www.whateverorigin.org/get?url=" +
-        encodeURIComponent(
-          "http://vindsiden.no/xml.aspx?id=" + this.state.spotId + "&callback=?"
-        ),
-      dataType: "jsonp",
-      success: function(data) {
-        let XMLParser = require("react-xml-parser");
-        let xml = new XMLParser().parseFromString(data.contents.toString());
-        console.log(xml);
-        console.log(xml.children[1]);
-        console.log(xml.children.length);
-
-        let tempData = [];
-        let i;
-        for (i = xml.children.length - 1; i > 0; i--) {
-          tempData.push({
-            time: xml.children[i].children[2].value,
-            windMin: xml.children[i].children[7].value,
-            windAvg: xml.children[i].children[3].value,
-            windMax: xml.children[i].children[6].value,
-            windDirection: xml.children[i].children[8].value,
-            temp: xml.children[i].children[11].value
-          });
-        }
-
-        this.setState({
-          name: xml.name,
-          windData: tempData
-        });
-      }.bind(this)
-    });
+  render() {
+    return this.getKiteReport();
   }
 }
 
